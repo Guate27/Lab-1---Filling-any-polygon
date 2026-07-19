032c2c036a9be3fc77132e674720c0ec6fa74366
Guate27 <reypellecer@gmail.com>
2026-07-18 19:34:58 -0600

    configuración inicial del proyecto

---
+++ lab_1/src/main.rs
@@ -0,0 +1,22 @@
+mod framebuffer;
+mod line;
+mod bmp;
+
+use crate::framebuffer::Framebuffer;
+use crate::line::Line;
+use crate::bmp::WriteBmp;
+
+fn main() {
+    let mut framebuffer = Framebuffer::new(800, 600);
+
+    framebuffer.set_background_color(0x000000);
+    framebuffer.clear();
+
+    framebuffer.set_current_color(0xFFFFFF);
+
+    
+
+    let _ = framebuffer.render_buffer("output.bmp");
+
+    println!("Framebuffer rendered to output.bmp");
+} 
