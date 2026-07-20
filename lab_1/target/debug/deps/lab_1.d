f8b9f3a37917f8da785d3e67170e6eda1c2c12a6
Guate27 <reypellecer@gmail.com>
2026-07-19 21:00:32 -0600

    Deja de trackear target, output.bmp y Cargo.lock; actualiza Cargo.toml

--- lab_1/target/debug/deps/lab_1.d
+++
@@ -1,8 +0,0 @@
-C:\Users\reype\OneDrive\Documentos\REYNALDO\UVG\PROGRAS\Gráficas\Lab-1---Filling-any-polygon\lab_1\target\debug\deps\lab_1.d: src\main.rs src\framebuffer.rs src\line.rs src\bmp.rs
-
-C:\Users\reype\OneDrive\Documentos\REYNALDO\UVG\PROGRAS\Gráficas\Lab-1---Filling-any-polygon\lab_1\target\debug\deps\lab_1.exe: src\main.rs src\framebuffer.rs src\line.rs src\bmp.rs
-
-src\main.rs:
-src\framebuffer.rs:
-src\line.rs:
-src\bmp.rs:
